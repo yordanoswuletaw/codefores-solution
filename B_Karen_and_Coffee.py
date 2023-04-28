e532a8f9b4cda11e05f44cff31f43e45558f536e
J0RdN-w <yordanoswuletaw@gmail.com>
2023-04-28 15:20:49 +0300

    update problems

---
+++ B_Karen_and_Coffee.py
@@ -0,0 +1,38 @@
+def main():
+    n, k, q = map(int, input().split())
+    recipes = []
+    quries = []
+    MAX_RANGE = 200002
+
+    for i in range(n):
+        recipes.append(tuple(map(int, input().split())))
+    
+    for i in range(q):
+        quries.append(tuple(map(int, input().split())))
+    
+    # creating exclusive prefix sum of n + 2 to inbound first and last element
+    prfxSum = [0] * MAX_RANGE
+    for start, end in recipes:
+        prfxSum[start] += 1
+        prfxSum[(end + 1)] -= 1
+    
+    # prefixsum to check if the recipe is admissible
+    for i in range(1, MAX_RANGE):
+        prfxSum[i] += prfxSum[i - 1]
+    
+    # marking admissible recipes with 1 and others with 0
+    for i in range(MAX_RANGE):
+        prfxSum[i] //= k
+    
+    # running other prefixsum to get the range of admissible recipes 
+    for i in range(1, MAX_RANGE):
+        prfxSum[i] += prfxSum[i - 1]
+    
+    # print admissable recipes in a given range
+    for start, end in quries:
+        print(prfxSum[end] - prfxSum[start - 1])
+    
+
+if __name__ == '__main__':
+    main()
+
