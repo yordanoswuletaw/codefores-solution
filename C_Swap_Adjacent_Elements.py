0a7cf2fdd629b474611814598cf6dc0d69256398
J0RdN-w <yordanoswuletaw@gmail.com>
2023-05-29 16:53:57 +0300

    update solution

--- C_Swap_Adjacent_Elements.py
+++ C_Swap_Adjacent_Elements.py
@@ -1,25 +1,17 @@
-from collections import deque
-
 def main():
     n = int(input())
     nums = list(map(int, input().split()))
-    rules = list(map(int, input()))
-    prfxSum = [0]
-
-    for i in range(len(rules)):
-        prfxSum.append(prfxSum[-1] + rules[i])
+    rules = input()
 
     ans = 'YES'
+    currMax = 0
     for i in range(n - 1):
-        if nums[i] > i + 1 and rules[i] == 0:
+        currMax = max(currMax, nums[i])
+        if currMax > i + 1 and rules[i] == '0':
             ans = 'NO'
             break
-        elif nums[i] > i + 1 and rules[i]:
-            if prfxSum[nums[i] - 1] - prfxSum[i - 1] < nums[i] - i - 1:
-                ans='NO'
-                break
+
     print(ans)
 
 if __name__ == '__main__':
-
     main()
